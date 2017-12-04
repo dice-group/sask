0db6813a608b07dd399ac946d5704e4a865a2520
AndreSonntag <andre-sonntag@gmx.de>
2017-12-04 17:34:56 +0100

    complete RestController + first tests

--- fred-ms/src/main/java/org/dice_research/sask/fred_ms/FredMsController.java
+++ fred-ms/src/main/java/org/dice_research/sask/fred_ms/FredMsController.java
@@ -1,29 +1,58 @@
 package org.dice_research.sask.fred_ms;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collections;
+
+import javax.ws.rs.QueryParam;
+
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+/**
+ * 
+ * @author André Sonntag
+ *
+ */
 @RestController
 public class FredMsController {
-	protected Logger logger = Logger.getLogger(FredMsController.class.getName());
-	
-	
-	@RequestMapping(value = "/info", produces = "application/rdf+json", method = RequestMethod.GET)
-	public String fred() {
+	private Logger logger = Logger.getLogger(FredMsController.class.getName());
+	private RestTemplate restTemplate = new RestTemplate();
+
+	@RequestMapping(value = "/extract", method = RequestMethod.GET)
+	public String extract(FredDTO fred){
+		
+		logger.info("FRED-microservice extract(FredDTO fred) invoked");
+		String input = "Miles Davis was an american jazz musician.";
+		//String input = "fred.getInput();"
+		String wfd = fred.getWfd_profile();
+		String annotation = fred.getTextannotation();
+		String format = fred.getFormat();
+
+		String host = "http://wit.istc.cnr.it/stlab-tools/fred";
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(host)
+				.queryParam("text", input)
+				.queryParam("wfd_profile", wfd)
+				.queryParam("textannotation", annotation);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("accept", format);
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		ResponseEntity<String> result = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);
 			
-		String text = "Miles Davis was an american jazz musician.";
-		String wfd = "b";
-		String annotation = "earmark";
-		
-		String uri = "http://wit.istc.cnr.it/stlab-tools/fred?text="+text+"&wfd_profile="+wfd+"&textannotation="+annotation;
-		RestTemplate rest = new RestTemplate();
-		String ret = rest.getForObject(uri, String.class);
-		
-		return ret;
+		return result.getBody();
 	}
-	
-	
+
 }
