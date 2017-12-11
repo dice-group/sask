93d5bb0219f5f44c767fa7325ba31fdd25bba354
AndreSonntag <andre-sonntag@gmx.de>
2017-12-11 16:52:42 +0100

    fix bugs in fred controller (double url parsing)

--- fred-ms/src/main/java/org/dice_research/sask/fred_ms/FredMsController.java
+++ fred-ms/src/main/java/org/dice_research/sask/fred_ms/FredMsController.java
@@ -1,7 +1,9 @@
 package org.dice_research.sask.fred_ms;
 
 import java.io.IOException;
+
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -13,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -27,16 +31,14 @@ public class FredMsController {
 	private Logger logger = Logger.getLogger(FredMsController.class.getName());
 	
 	private RestTemplate restTemplate = new RestTemplate();
-
-	@GetMapping(value = "/extract", consumes=MediaType.APPLICATION_JSON_VALUE)
-	public String extract(@RequestBody FredDTO fred){
+	@GetMapping(value = "/extract", produces = "text/rdf+n3" ,consumes=MediaType.APPLICATION_JSON_VALUE)
+	 	public String extract(@RequestBody FredDTO fred){
 		logger.info("FRED-microservice extract(FredDTO fred) invoked");
 		
 		if(fred == null) {
 		      throw new NullPointerException("No input");
 		}
-		
-		//String input = "Miles Davis was an american jazz musician.";
+			
 		String input = fred.getInput();
 		String wfd = fred.getWfd_profile();
 		String annotation = fred.getTextannotation();
@@ -47,14 +49,15 @@ public class FredMsController {
 		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(host)
 				.queryParam("text", input)
 				.queryParam("wfd_profile", wfd)
-				.queryParam("textannotation", annotation);
-
+				.queryParam("textannotation", annotation)
+				.queryParam("format", format);
+		String uri = builder.build().toUriString();
+	
 		//Build header for FRED request
 		HttpHeaders headers = new HttpHeaders();
-		headers.set("accept", format);
+		headers.set("accept",format);
 		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		ResponseEntity<String> result = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);
-			
+		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
 		return result.getBody();
 	}
 	
@@ -63,5 +66,5 @@ public class FredMsController {
 		logger.error("FRED-microservice NullPointerException");
 	    response.sendError(HttpStatus.BAD_REQUEST.value());
 	  }
-
+	
 }
