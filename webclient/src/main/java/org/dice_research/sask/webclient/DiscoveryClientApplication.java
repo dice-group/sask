74db6250fe7e6267356bf9ac8508bd50483d2312
Prashanth H S <shadows123@Prashanths-MacBook-Pro.local>
2017-12-14 21:53:56 +0100

    Support for Zuul to dynamically route UI POST Requests to Spring Controller present on another port.

--- webclient/src/main/java/org/dice_research/sask/webclient/DiscoveryClientApplication.java
+++ webclient/src/main/java/org/dice_research/sask/webclient/DiscoveryClientApplication.java
@@ -3,9 +3,11 @@ package org.dice_research.sask.webclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class DiscoveryClientApplication 
 {
 	public static void main(String[] args) {
