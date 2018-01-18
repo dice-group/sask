b0ebe4d09bef22613be0485276ffb343788383b4
guru <guru5590@gmail.com>
2018-01-18 22:19:05 +0100

    Refactored the Rivescript code for performance, following conventions and added support for template based response which in future along with UI chnages would be used as inital messages from chatbot

---
+++ chatbot/src/main/java/chatbot/core/handlers/rivescript/RiveScriptQueryHandler.java
@@ -0,0 +1,41 @@
+package chatbot.core.handlers.rivescript;
+
+import org.apache.log4j.Logger;
+import java.util.*;
+import com.rivescript.RiveScript;
+
+import chatbot.io.incomingrequest.IncomingRequest;
+import chatbot.io.response.Response;
+import chatbot.io.response.ResponseList;
+import chatbot.core.handlers.*;
+import java.io.IOException;
+
+public class RiveScriptQueryHandler extends Handler {
+	private static Logger log = Logger.getLogger(RiveScriptQueryHandler.class.getName());
+
+	//Rivescript reponse is now handled in RiveScriptOutputHandler
+	
+	public ResponseList search(IncomingRequest request) throws IOException {
+		try {
+			// Analyze passed Output from Rivescript for whether further
+			// processing is required.
+			ResponseList responselist = new ResponseList();
+			RiveScriptOutputAnalyzer analyzer = new RiveScriptOutputAnalyzer();
+			String query = request.getRequestContent()
+			                      .get(0)
+			                      .getText()
+			                      .toLowerCase();
+			responselist = analyzer.riveHandler(query);
+			return responselist;
+
+		} catch (Exception e) {
+			// Check if we can create a logger.
+			log.error("search, Exception in handling Rivescript Queries");
+			ResponseList responselist = new ResponseList();
+			responselist.setError();
+			return responselist;
+		}
+
+	}
+
+}
