e2cc7f47792242db72474867edf58a84cef3a6dc
K Haack <khaack@campus.uni-paderborn.de>
2018-01-21 21:02:34 +0100

    added commandstack

---
+++ webclient/src/main/resources/static/js/workspace/CommandStack.js
@@ -0,0 +1,93 @@
+var CommandStack = function(options){
+	
+	var root = this;
+	
+	var vars = {
+		commands : [],
+		index : undefined
+    };
+	
+	var logError = function (message) {
+		if (window.console) {
+			window.console.error(message);
+		}
+	};
+	
+	/**
+     * Constructor
+     */
+    this.construct = function(options){
+        $.extend(vars , options);
+    };
+    
+    this.construct(options);
+	
+	/**
+	 * Do the command.
+	 */
+	this.doCommand = function(commandArray) {
+		if(!commandArray || !commandArray['do'] || typeof commandArray['do'] !== 'function') {
+			logError("the command does not have a 'do' function");
+		}
+		
+		var command = commandArray['do'];
+		command();
+		
+		if(commandArray['undo'] && typeof commandArray['undo'] !== 'function') {
+			commands.push(commandArray);
+			index = commands.length - 1;
+		}
+	};
+	
+	/**
+	 * Undo the last command.
+	 */
+	this.undoCommand = function() {
+		var commandArray = commands[index];
+		
+		if(!commandArray) {
+			logError("no commands for undo");
+		}
+		
+		var command = commandArray['undo'];
+		command();
+		index -= 1;
+	};
+	
+	/**
+	 * Redo the next command.
+	 */
+	this.redoCommand = function() {
+		var commandArray = commands[index + 1];
+		
+		if(!commandArray) {
+			logError("no commands for redo");
+		}
+		
+		var command = commandArray['do'];
+		command();
+		index += 1;
+	};
+	
+	/**
+	 * Clear the commands.
+	 */
+	this.clear = function() {
+		commands = [];
+		index = undefined;
+	};
+	
+	/**
+	 * Has command for undo.
+	 */
+	this.hasUndo = function () {
+        return index !== -1;
+	};
+	
+	/**
+	 * Has command for redo.
+	 */
+	this.hasRedo = function () {
+        return index < (commands.length - 1);
+	};
+};
