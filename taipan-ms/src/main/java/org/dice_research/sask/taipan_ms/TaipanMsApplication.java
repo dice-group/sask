fcf037e2bb1fcda145b46aed8e7ecaab6b13302f
AndreSonntag <Andre-Sonntag@gmx.de>
2018-05-07 14:50:15 +0200

    adapt the code for taipan

--- taipan-ms/src/main/java/org/dice_research/sask/taipan_ms/TaipanMsApplication.java
+++ taipan-ms/src/main/java/org/dice_research/sask/taipan_ms/TaipanMsApplication.java
@@ -1,20 +1,13 @@
 package org.dice_research.sask.taipan_ms;
 
-import org.dice_research.sask.taipan_ms.WorkflowDeserializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 
 /**
  * This class is responsible for launching the microservice.
  * 
- * @author Kevin Haack
+ * @author André Sonntag
  *
  */
 @SpringBootApplication
@@ -24,11 +17,6 @@ public class TaipanMsApplication {
 		SpringApplication.run(TaipanMsApplication.class, args);
 	}
 
-	@Bean
-	@LoadBalanced
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 
 
 }
