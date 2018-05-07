8a9e59bfce4ff33cd8070c1e345a35732af3e31e
Juzerhk <juzerhk@mail.uni-paderborn.de>
2018-05-08 00:28:28 +0200

    removed unwanted brackets

--- chatbot/src/main/java/chatbot/core/handlers/rivescript/RiveScriptOutputAnalyzer.java
+++ chatbot/src/main/java/chatbot/core/handlers/rivescript/RiveScriptOutputAnalyzer.java
@@ -75,7 +75,7 @@ public class RiveScriptOutputAnalyzer {
 
 		String responseHandleText = null;
 
-		if (isJSONValid(textMessage) && (textMessage.length() > 0 && textMessage.split("\\s+").length!=1) ) {
+		if (isJSONValid(textMessage) && textMessage.length() > 0 && textMessage.split("\\s+").length!=1 )  {
 			JsonObject jsonInputText = new JsonObject().getAsJsonObject(textMessage);
 			if (jsonInputText.has("type")) {
 				String typeValue = jsonInputText.get("name").getAsString();
