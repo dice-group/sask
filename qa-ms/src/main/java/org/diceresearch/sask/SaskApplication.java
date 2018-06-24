3b61022c0306279674caeb68ffa8575dbc1c7308
sajjadpz <sajjad.pervaiz@hotmail.com>
2018-06-24 18:42:00 +0200

    added qa-ms base structure

---
+++ qa-ms/src/main/java/org/diceresearch/sask/SaskApplication.java
@@ -0,0 +1,12 @@
+package org.diceresearch.sask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaskApplication.class, args);
+	}
+}
