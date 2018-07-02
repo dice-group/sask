4af22bf005e77aa4c168a124d05746437aa312cb
sajjadpz <sajjad.pervaiz@hotmail.com>
2018-07-02 14:32:31 +0200

    Added base code to make a RESTful api call

--- qa-ms/src/main/java/org/diceresearch/sask/qa/QaMsController.java
+++ qa-ms/src/main/java/org/diceresearch/sask/qa/QaMsController.java
@@ -1,8 +1,25 @@
 package org.diceresearch.sask.qa;
 
+import org.diceresearch.sask.integration.SurniaQAService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
+@RequestMapping("/qa")
 public class QaMsController {
 
+    private final SurniaQAService surniaQAService;
+
+    public QaMsController(SurniaQAService surniaQAService) {
+        this.surniaQAService = surniaQAService;
+    }
+
+    @GetMapping("/{query}")
+    public String getAnswerFromSurnia(@Valid @PathVariable String query) {
+        return surniaQAService.askSurnia(query);
+    }
 }
