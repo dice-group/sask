e0dec24ad120011e88f6651da009705da01641f2
Nesara <39854185+naturecreator@users.noreply.github.com>
2018-11-08 05:45:15 +0100

    Create SparqlQuery.java

---
+++ qans-ms/SparqlQuery.java
@@ -0,0 +1,40 @@
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.FileManager;
+
+import java.io.*;
+
+
+public class SparqlQuery extends Object{
+
+    static final String inputFileName = "new 2.ttl";
+
+    public static void main(String args[]){
+
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+
+        // use the FileManager to find the input file
+        InputStream in = FileManager.get().open( inputFileName );
+        if (in == null) {
+            throw new IllegalArgumentException(
+                    "File: " + inputFileName + " not found");
+        }
+
+// read the RDF/XML file
+        model.read(inputFileName);
+
+        String sparqlQuery =
+
+                "Prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                        + " SELECT ?uri" +
+                        "WHERE {" +
+                        "  ?uri  dbo:trainer  dbr:Conor_McGregor." +
+                        "      }";
+
+// write it to standard out
+        model.write(System.out);
+
+    }
+
+}
