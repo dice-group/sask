10491d7d8929c8082c4f2f9f25c781e2c7333a10
hjshah142 <harshshah142@gmail.com>
2018-11-25 03:08:20 +0100

    Shorten the main method

--- ensemble-ms/src/main/java/org/dice_research/sask/ensemble_ms/arff_generator/SentenceExtractor.java
+++ ensemble-ms/src/main/java/org/dice_research/sask/ensemble_ms/arff_generator/SentenceExtractor.java
@@ -50,160 +50,176 @@ public class SentenceExtractor {
 					return Integer.valueOf(s1).compareTo(Integer.valueOf(s2));
 				}
 			});
-
-//			Initialize value with null
-			String fox_response_string = null;
-			String openie_response_string = null;
-			String sorokin_response_string = null;
-			String cedric_response_string = null;
-
 			for (int i = 0; i < 3; i++) 
 			{
-				if (files[i].isFile()) {
-					String sentence = readLineByLine(files[i].toString());
-					int x = sentence.indexOf("nif:isString   ");
-					int start = x + 17;
-					int end = sentence.indexOf("\" .");
-					end--;
-					sentence_data = sentence.substring(start, end);
-					// System.out.println(sentence_data);
-					sentences.add(sentence_data);					
-//				Extractors responses
+			sentence_Extracion(i);
+			}
+			
+	}
+
+
+
+ private static void sentence_Extracion(int i) 
+ 	{
+//		Initialize value with null
+		String fox_response_string = null;
+		String openie_response_string = null;
+		String sorokin_response_string = null;
+		String cedric_response_string = null;
+
+		
+		
+			if (files[i].isFile()) {
+				String sentence = readLineByLine(files[i].toString());
+				int x = sentence.indexOf("nif:isString   ");
+				int start = x + 17;
+				int end = sentence.indexOf("\" .");
+				end--;
+				sentence_data = sentence.substring(start, end);
+				// System.out.println(sentence_data);
+				sentences.add(sentence_data);					
+//			Extractors responses
 //
-//					Map<String, Integer> port_vs_extractorMap = new HashMap<String, Integer>();
-//					port_vs_extractorMap.put("fox", 2222);
-//					port_vs_extractorMap.put("fred", 2223);
-//					port_vs_extractorMap.put("spotlight", 2224);
-//					port_vs_extractorMap.put("cedric", 2225);
-//					port_vs_extractorMap.put("openIE", 2226);
-//					port_vs_extractorMap.put("sorookin", 2227);
+//				Map<String, Integer> port_vs_extractorMap = new HashMap<String, Integer>();
+//				port_vs_extractorMap.put("fox", 2222);
+//				port_vs_extractorMap.put("fred", 2223);
+//				port_vs_extractorMap.put("spotlight", 2224);
+//				port_vs_extractorMap.put("cedric", 2225);
+//				port_vs_extractorMap.put("openIE", 2226);
+//				port_vs_extractorMap.put("sorookin", 2227);
 
-					// int portNumb[] = { 2222, 2224, 2225, 2226, 2227 };
-					int portNumb[] = { 2222, 2226, 2227, 2225 };
+				// int portNumb[] = { 2222, 2224, 2225, 2226, 2227 };
+				int portNumb[] = { 2222, 2226, 2227, 2225 };
 
 
-					Map<String, String> fredRespMap = new HashMap<String, String>();
-					Map<String, String> spotlightRespMap = new HashMap<String, String>();
-					Map<String, String> cedricRespMap = new HashMap<String, String>();
-					Map<String, String> openIERespMap = new HashMap<String, String>();
-					Map<String, String> sorookinRespMap = new HashMap<String, String>();
+				Map<String, String> fredRespMap = new HashMap<String, String>();
+				Map<String, String> spotlightRespMap = new HashMap<String, String>();
+				Map<String, String> cedricRespMap = new HashMap<String, String>();
+				Map<String, String> openIERespMap = new HashMap<String, String>();
+				Map<String, String> sorookinRespMap = new HashMap<String, String>();
 
-					// calling every extractors and getting outputs
+				// calling every extractors and getting outputs
 
-					for (int j = 0; j < portNumb.length; j++) {
-//      To handle url_encoding exception
-						try {
-						// replacing url with space character
-						String URL2 = java.net.URLEncoder.encode(sentences.get(i), "UTF-8").replace("+", "%20");
-						// add extractor for url
-						String _extractorURL = "http://localhost:" + portNumb[j] + "/extractSimple?input=" + URL2;
-						// java.awt.Desktop.getDesktop().browse(java.net.URI.create(_extractorURL));
-						// _extractorURL = "https://www.google.com/";
+				for (int j = 0; j < portNumb.length; j++) {
+//To handle url_encoding exception
+					try {
+					// replacing url with space character
+					String URL2 = java.net.URLEncoder.encode(sentences.get(i), "UTF-8").replace("+", "%20");
+					// add extractor for url
+					String _extractorURL = "http://localhost:" + portNumb[j] + "/extractSimple?input=" + URL2;
+					// java.awt.Desktop.getDesktop().browse(java.net.URI.create(_extractorURL));
+					// _extractorURL = "https://www.google.com/";
 
-						URL url;
-	
-							url = new URL(_extractorURL);
-							//
-							HttpURLConnection con = (HttpURLConnection) url.openConnection();
-							// System.out.println(url);
-							con.setRequestMethod("GET");
-							BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-							String inputLine;
-							StringBuffer response = new StringBuffer();
+					URL url;
 
-							while ((inputLine = in.readLine()) != null) {
+						url = new URL(_extractorURL);
+						//
+						HttpURLConnection con = (HttpURLConnection) url.openConnection();
+						// System.out.println(url);
+						con.setRequestMethod("GET");
+						BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+						String inputLine;
+						StringBuffer response = new StringBuffer();
 
-								response.append(inputLine);
-							}
+						while ((inputLine = in.readLine()) != null) {
 
-							in.close();
+							response.append(inputLine);
+						}
 
-							// print result from response
+						in.close();
 
-							if (j == 0) 
-							{
-								List<String> fox_response_string_list = new ArrayList<String>();
-								fox_response_string = response.toString();
-								fox_response_string_list.add(fox_response_string);
-							}
+						// print result from response
 
-							else if (j == 1) 
-							{
-								fredRespMap.put(sentences.get(i), response.toString());
-								ArrayList<String> openie_response_string_list = new ArrayList<String>();
-								openie_response_string = response.toString();
-								openie_response_string_list.add(openie_response_string);
-							}
+						if (j == 0) 
+						{
+							List<String> fox_response_string_list = new ArrayList<String>();
+							fox_response_string = response.toString();
+							fox_response_string_list.add(fox_response_string);
+							fox_response_processing(fox_response_string);
+						}
 
-							else if (j == 2) 
-							{
-								spotlightRespMap.put(sentences.get(i), response.toString());
-								ArrayList<String> sorokin_response_string_list = new ArrayList<String>();
-								sorokin_response_string = response.toString();
-								sorokin_response_string_list.add(sorokin_response_string);
-							}
-							else if (j == 3) 
-							{
-								cedricRespMap.put(sentences.get(i), response.toString());
-								ArrayList<String> cedric_response_string_list = new ArrayList<String>();
-								cedric_response_string = response.toString();
-								cedric_response_string_list.add(cedric_response_string);								
+						else if (j == 1) 
+						{
+							fredRespMap.put(sentences.get(i), response.toString());
+							ArrayList<String> openie_response_string_list = new ArrayList<String>();
+							openie_response_string = response.toString();
+							openie_response_string_list.add(openie_response_string);
+						}
 
-							}
-							else if (j == 4) {
-								openIERespMap.put(sentences.get(i), response.toString());
-							}
-							else if (j == 5) 
-							{
-								sorookinRespMap.put(sentences.get(i), response.toString());
-							}
+						else if (j == 2) 
+						{
+							spotlightRespMap.put(sentences.get(i), response.toString());
+							ArrayList<String> sorokin_response_string_list = new ArrayList<String>();
+							sorokin_response_string = response.toString();
+							sorokin_response_string_list.add(sorokin_response_string);
+						}
+						else if (j == 3) 
+						{
+							cedricRespMap.put(sentences.get(i), response.toString());
+							ArrayList<String> cedric_response_string_list = new ArrayList<String>();
+							cedric_response_string = response.toString();
+							cedric_response_string_list.add(cedric_response_string);								
 
 						}
-						// end of try block
-						catch (Exception e) {
-							// TODO: handle exception
+						else if (j == 4) {
+							openIERespMap.put(sentences.get(i), response.toString());
+						}
+						else if (j == 5) 
+						{
+							sorookinRespMap.put(sentences.get(i), response.toString());
 						}
 
 					}
-					
-
-	
-					System.out.println("Extracted sentence From the file " + files[i].getName());	
-
-					System.out.println(sentences.get(i));
-					System.out.println("Fox extractor response for file" + files[i].getName());			
-					System.out.println(fox_response_string);
-					System.out.println("OpenIE extractor response for file " + files[i].getName());			
-					System.out.println(openie_response_string);
-					System.out.println("Cedric extractor response for file"  + files[i].getName());		
-					System.out.println(cedric_response_string);
-					System.out.println("Sorokin extractor response for file " + files[i].getName());				
-					System.out.println(sorokin_response_string);
-					System.out.println("Gethering Sparql Query Result................................");
-					
-					String squery_Result = responseReader(i);
-					System.out.println("......................................");
-					System.out.println(squery_Result);
-					
-					System.out.println("Training Data for file " + files[i].getName());		
-					String training_data = " ' " + sentences.get(i) + " ' " + ", " + "'" + fox_response_string + " ' "
-							+ "," + " ' " + openie_response_string + " ' " + "," + " ' " + sorokin_response_string + ","
-							+ " ' " + cedric_response_string +   " ' " +  squery_Result + " ' ";
-					
-					System.out.println("------");
-					System.out.println(training_data);
-
-					System.out.println("-------------------------------------------------------------");
-					trainingFileWriter(training_data);
-
+					// end of try block
+					catch (Exception e) {
+						// TODO: handle exception
+					}
 
 				}
+				
+
+
+				System.out.println("Extracted sentence From the file " + files[i].getName());	
+
+				System.out.println(sentences.get(i));
+				System.out.println("Fox extractor response for file" + files[i].getName());			
+				System.out.println(fox_response_string);
+				System.out.println("OpenIE extractor response for file " + files[i].getName());			
+				System.out.println(openie_response_string);
+				System.out.println("Cedric extractor response for file"  + files[i].getName());		
+				System.out.println(cedric_response_string);
+				System.out.println("Sorokin extractor response for file " + files[i].getName());				
+				System.out.println(sorokin_response_string);
+				System.out.println("Gethering Sparql Query Result................................");
+				
+				String squery_Result = responseReader(i);
+				System.out.println("......................................");
+				System.out.println(squery_Result);
+				
+				System.out.println("Training Data for file " + files[i].getName());		
+				String training_data = " ' " + sentences.get(i) + " ' " + ", " + "'" + fox_response_string + " ' "
+						+ "," + " ' " + openie_response_string + " ' " + "," + " ' " + sorokin_response_string + ","
+						+ " ' " + cedric_response_string +   " ' " +  squery_Result + " ' ";
+				
+				System.out.println("------");
+				System.out.println(training_data);
+
+				System.out.println("-------------------------------------------------------------");
+				trainingFileWriter(training_data);
+
 
 			}
 
 		
 
+	
+
+	 
+		// TODO Auto-generated method stub
+		
 	}
+
+
 
 
 
@@ -343,6 +359,13 @@ public class SentenceExtractor {
 		
 	
 		}
+
+	private static void fox_response_processing(String fox_response_string) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 	
 
