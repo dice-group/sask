abf5b9239b164d56474ddf2fef14ef5125c959f0
hjshah142 <harshshah142@gmail.com>
2018-12-03 00:07:43 +0100

    write result in N-triple format

--- ensemble-ms/src/main/java/org/dice_research/sask/ensemble_ms/arff_generator/SentenceExtractor.java
+++ ensemble-ms/src/main/java/org/dice_research/sask/ensemble_ms/arff_generator/SentenceExtractor.java
@@ -54,9 +54,9 @@ public class SentenceExtractor {
 					return Integer.valueOf(s1).compareTo(Integer.valueOf(s2));
 				}
 			});
-			 
+//			 number of ttl files
 			System.out.println(files.length);
-			for (int i = 0; i < files.length; i++) 
+			for (int i = 0; i < 5; i++) 
 			{
 			se.sentence_Extracion(i);
 			System.out.println("Extracted sentence From the file " + files[i].getName());	
